78c71ca1d58e9afdb0678ee762236f272a6235d3
Du-devBR <eduardo.ananias2@hotmail.com>
2022-05-05 21:09:32 -0300

    criado setup para backend da aplicação.Configurado: Typescript, ts-node-dev,  rota principal com express, instalado prisma para modeldo banco SQLLITE

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    return res.send("hello word")
+})
+
+app.listen(3333, () => {
+    console.log('HTTP running')
+})
